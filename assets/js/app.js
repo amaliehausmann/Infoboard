bd12f3832e418062f9dfa568e0904a0e88bd79d9
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-04-04 01:43:40 +0200

    Vejr funktioner v2

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 
 
 // const myWeather = document.getElementById('weather');
+const apiKey = '4d58d6f0a435bf7c5a52e2030f17682d'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Aalborg&units=metric';
+
 
 
 getWeatherData();
@@ -9,7 +12,8 @@ getWeatherData();
 
 function getWeatherData() {
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Aalborg&appid=4d58d6f0a435bf7c5a52e2030f17682d&units=metric')
+    fetch(`${apiUrl}&appid=${apiKey}`)
+
 
         .then(res => res.json())
 
@@ -22,19 +26,32 @@ function receivedWeatherData(weatherData) {
 
     // Udtræk relevante oplysninger fra API svaret
     let temperature = weatherData.main.temp;
-    let weatherDescription = weatherData.weather[0].description;
+    let weatherIcon = weatherData.weather[0].icon;
+
 
     displayTemperature(temperature);
-    displayWeatherIcon(weatherDescription);
+    displayWeatherIcon(weatherIcon);
 
     // Vis vejrobservationerne i konsollen (eller du kan ændre dette til at vise dem på din webside)
-    console.log("Temperatur: " + temperature + " °C");
-    console.log("Beskrivelse: " + weatherDescription);
 
 }
 
 
 
 /*VIEW CODE--------------------------------------------------------------------------------------------*/
+
+function displayTemperature(temperature) {
+    const temperatureElement = document.getElementById('temperature');
+    temperatureElement.textContent = `Temperatur: ${temperature} °C`;
+}
+
+function displayWeatherIcon(weatherIcon) {
+    const iconElement = document.getElementById('weatherIcon');
+
+    const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}.png`
+
+    iconElement.setAttribute('src', iconUrl);
+
+}
 
 
