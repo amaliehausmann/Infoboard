ccc7b2ec9773a667b1842787da47cbd4e904279a
Razzoy <1081892@ucn.dk>
2024-04-04 10:37:37 +0200

    #25 #10 #3 #18
    Updated and done? Added correct svg

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,6 +1,4 @@
 
-
-// const myWeather = document.getElementById('weather');
 const apiKey = '4d58d6f0a435bf7c5a52e2030f17682d'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Aalborg&units=metric';
 const myClock = document.getElementById('clock');
@@ -76,9 +74,37 @@ function displayTemperature(temperature) {
 function displayWeatherIcon(weatherIcon) {
     const iconElement = document.getElementById('weatherIcon');
 
-    const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}.png`
+    // Mappning af vejr-ikonkoder til SVG-filnavne
+    const iconMappings = {
+        '01d': 'clear.svg',
+        '01n': 'clear-night.svg',
+        '02d': 'few-clouds.svg',
+        '02n': 'few-clouds-night.svg',
+        '03d': 'scattered-clouds.svg',
+        '03n': 'scattered-clouds.svg',
+        '04d': 'broken-clouds.svg',
+        '04n': 'broken-clouds.svg',
+        '09d': 'shower-rain.svg',
+        '09n': 'shower-rain.svg',
+        '10d': 'rain.svg',
+        '10n': 'rain.svg',
+        '11d': 'thunderstorm.svg',
+        '11n': 'thunderstorm.svg',
+        '13d': 'snow.svg',
+        '13n': 'snow.svg',
+        '50d': 'mist.svg',
+        '50n': 'mist.svg'
+    };
 
-    iconElement.setAttribute('src', iconUrl);
+    // Hent SVG-filnavnet fra mappningen
+    const iconFileName = iconMappings[weatherIcon];
+
+    if (iconFileName) {
+        const iconUrl = `assets/images/${iconFileName}`;
+        iconElement.setAttribute('src', iconUrl);
+    } else {
+        console.error('Ukendt vejr-ikon:', weatherIcon);
+    }
 
 }
 
