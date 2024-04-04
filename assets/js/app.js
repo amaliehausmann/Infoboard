446510fb9f9ac031d1cf4f61b86772ca81dbb769
Razzoy <1081892@ucn.dk>
2024-04-04 08:32:10 +0200

    Addition of time

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,10 +3,14 @@
 // const myWeather = document.getElementById('weather');
 const apiKey = '4d58d6f0a435bf7c5a52e2030f17682d'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Aalborg&units=metric';
-
-
+const myClock = document.getElementById('clock');
+const myDate = document.getElementById('date');
 
 getWeatherData();
+
+// Opdaterer uret hvert sekund
+setInterval(getDate, 1000);
+getDate();
 
 /*MODEL CODE---------------------------------------------------------------------------------------------------*/
 
@@ -36,7 +40,28 @@ function receivedWeatherData(weatherData) {
 
 }
 
+function getDate() {
+    let currentTime = new Date();
+    let hours = currentTime.getHours();
+    let minutes = currentTime.getMinutes();
+    let seconds = currentTime.getSeconds();
 
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    myClock.innerText = hours + ":" + minutes + ":" + seconds;
+
+    let day = currentTime.toLocaleString('default', { weekday: 'long' }); // Få ugedagen som tekst
+    let date = currentTime.getDate();
+    let month = currentTime.toLocaleString('default', { month: 'long' });
+
+    myDate.innerText = day + " | " + date + ". " + month;
+
+}
 
 /*VIEW CODE--------------------------------------------------------------------------------------------*/
 
